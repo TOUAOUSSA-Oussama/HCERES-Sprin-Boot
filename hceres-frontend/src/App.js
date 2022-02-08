ad84da842f9b76eefd0b8e7c68903815dd6cf187
Aloi <elaloi.otmane@gmail.com>
2022-02-08 14:09:10 +0100

    Create Form front end

--- hceres-frontend/src/App.js
+++ hceres-frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Researcher from './components/Researcher/Researcher';
-import Activity from './components/Activity/Activity';
+import Education from './components/Activity/Education/Education';
 import Footer from './components/Footer/Footer';
 import About from './components/About/About';
 
@@ -19,7 +19,7 @@ function App() {
       <Routes>
           <Route path='/Home' exact  element={<Home/>} />
           <Route path='/Researcher' exact  element={<Researcher/>} />
-          <Route path='/Activity' exact  element={<Activity/>} />
+          <Route path='/Activity' exact  element={<Education/>} />
           <Route path='/About' exact  element={<About/>} />
       </Routes> 
 
