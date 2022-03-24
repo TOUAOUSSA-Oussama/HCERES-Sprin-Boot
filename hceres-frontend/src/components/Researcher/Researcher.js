f3c77d617f49fe4c84da201794f4a678caf0336e
OUSSAMA TOUAOUSSA <touaoussa.oussama.tsi@gmail.com>
2022-03-24 11:32:10 +0100

    'finalResearchPage'

--- hceres-frontend/src/components/Researcher/Researcher.js
+++ hceres-frontend/src/components/Researcher/Researcher.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import Navbar from '../Navbar/Navbar';
 //import {Table} from "antd";
-import { Link } from "react-router-dom";
+import { generatePath } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Axios from 'axios'
+import { Link, useNavigate, useParams } from "react-router-dom";
+import UpdateResearcher from './UpdateResearcher';
+
+
 
 class Researcher extends Component {
     constructor() {
         super()
         this.state = {
             researchers: [],
-            loading: false
+            loading: false,
+            showUpdate: false,
+            idResearcher: 0,
         }
+        
+        this.handleUpdate = this.handleUpdate.bind(this);
     }
 
+    /*{showForm && (<AddResearcher> </AddResearcher>)}
+     onClick={setShowForm}
+    */
+    deleteResearcher(id) {
 
+        console.log(id);
+        Axios.delete(`http://localhost:9000/deleteResearcher/${id}`)
+            .then(res => {
+                window.location.reload(false);
+            })
+    }
+    
+    handleUpdate(idResearcher) {
+        this.setState({
+            showUpdate: true,
+            idResearcher: idResearcher
+        })
+    }
+    
     async componentDidMount() {
 
         const url = "http://localhost:9000/Researchers";
@@ -28,21 +55,30 @@ class Researcher extends Component {
     }
 
     render() {
+        if (this.state.showUpdate) {
+            return (
+                <UpdateResearcher id={this.state.idResearcher}/>
+            );
+        }
 
         if (this.state.researchers && this.state.researchers.length) {
-            //return this.state.researchers.map((chercheur, index) => {
 
             return (
-                <div>
-                    <Navbar />
 
+                <div>
                     <div className="container">
-                        <h3>Liste des chercheurs </h3>
+                        
+                                
+                                <h3>Liste des chercheurs </h3>
+                                <a href="/AddResearcher"  class="btn btn-success" role="button" data-bs-toggle="button">Ajouter un chercheur</a>
+                            
+                        
                         <hr />
+
                         <table className="table table-bordered table-striped" > <thead className="thead-dark">
                             <tr>
                                 <th>Nom</th>
-                                <th>Prenom</th>
+                                <th>Prénom</th>
                                 <th>Email</th>
                                 <th>Actions</th>
                             </tr>
@@ -55,9 +91,8 @@ class Researcher extends Component {
                                             <td>{chercheur.researcherName}</td>
                                             <td>{chercheur.researcherEmail}</td>
                                             <td>
-                                                <button className="btn btn-info" >Update</button>
-
-                                                <button className="btn btn-danger ml-2">Delete</button>
+                                                <button onClick={ () => { this.handleUpdate(chercheur.researcherId) }} class="btn btn-info" role="button" data-bs-toggle="button">Modifier</button>
+                                                <button className="btn btn-danger ml-2" onClick={() => { this.deleteResearcher(chercheur.researcherId) }} >Supprimer</button>
                                             </td>
                                         </tr>
                                     )}
@@ -72,35 +107,6 @@ class Researcher extends Component {
                     </div>
                 </div>
             );
-
-
-
-
-
-            /*
-                return (
-
-                    <div key={index}>
-
-                        <h3>{chercheur.researcherSurname}</h3>
-                        <p>{chercheur.researcherName}</p>
-                        <p>{chercheur.researcherEmail}</p>
-                    </div>
-
-               );
-            })
-             */
-            //const columns = [
-            //    {
-            //        title:"First name",
-            //        dataIndex: "researcherSurname"
-            //    }
-            //];
-
-            //return  ( 
-            //    <Table datasource={this.state.researchers} columns={columns} rowKey="researcherId" />
-            //);
-
         }
 
         return (
@@ -113,83 +119,3 @@ class Researcher extends Component {
 };
 
 export default Researcher;
-
-/*
-import { useEffect, useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-//import ResearcherService from "../services/Researcher.service";
-import { Link } from "react-router-dom";
-const ResearchersList = () => {
-
-    const [Researchers, setResearchers] = useState([]);
-
-    const init = () => {
-        ResearcherService.getAll()
-            .then(response => {
-                console.log('Printing Researchers data', response.data);
-                setResearchers(response.data);
-            })
-            .catch(error => {
-                console.log('Something went wrong', error);
-            })
-    }
-
-    useEffect(() => {
-        init();
-    }, []);
-
-    const handleDelete = (id) => {
-        console.log('Printing id', id);
-        ResearcherService.remove(id)
-            .then(response => {
-                console.log('Researcher deleted successfully', response.data);
-                init();
-            })
-            .catch(error => {
-                console.log('Something went wrong', error);
-            })
-    }
-
-    return (
-        <div className="container">
-            <h3>List of Researchers </h3>
-            <hr />
-            <div >
-
-                <Link to="/add" className="btn btn-primary mb-2"> Add Researcher</Link>
-                <table className="table table-bordered table-striped" >
-                    <thead className="thead-dark">
-                        <tr>
-                            <th>Nom</th>
-                            <th>Prenom</th>
-                            <th>Email</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            Researchers.map(Researcher =>
-                                <tr key={Researcher.id}>
-                                    <td>{Researcher.name}</td>
-                                    <td>{Researcher.location}</td>
-                                    <td>{Researcher.department}</td>
-                                    <td>
-                                        <Link className="btn btn-info" to={`/Researchers/edit/${Researcher.id}`}>Update</Link>
-
-                                        <button className="btn btn-danger ml-2" onClick={() => {
-                                            handleDelete(Researcher.id);
-                                        }}>Delete</button>
-                                    </td>
-                                </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
-    );
-}
-
-export default ResearchersList;
-*/
--- hceres/src/main/java/org/centrale/hceres/controller/ResearchController.java
+++ hceres/src/main/java/org/centrale/hceres/controller/ResearchController.java
@@ -1,11 +1,21 @@
 package org.centrale.hceres.controller;
 
+import java.util.Map;
+
 import org.centrale.hceres.items.Researcher;
+import org.centrale.hceres.repository.ResearchRepository;
 import org.centrale.hceres.service.ResearchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import java.util.Optional;
+
 
 // Controller permet de receptionner la requete http depuis le client, envoyer cette requete a service pour la traiter, puis retouner la reponse
 // la reponse sera sous format JSON (il s'agit d'une REST API)
@@ -18,6 +28,11 @@ public class ResearchController {
 	 */
 	@Autowired
 	private ResearchService rs;
+	/**
+	 * Instanciation de ResearchRepository
+	 */
+	@Autowired
+	private ResearchRepository researchRepo;
 	
 	/**
 	 * pour une requete GET dans localhost/researchs => fournir la liste des chercheurs
@@ -27,5 +42,61 @@ public class ResearchController {
 	@GetMapping("/Researchers")
 	public Iterable<Researcher> getResearchers() {
 		return rs.getResearchers();
+	}
+	
+	/**
+	 * Delete - Delete an element
+	 * @param id - The id of the element
+	 */
+	@DeleteMapping("/deleteResearcher/{id}")
+	
+	public void deleteEducation(@RequestBody @PathVariable("id") final Integer id) {
+		rs.deleteResearcher(id);
+	}
+	
+	/**
+	 * ajouter un elmt a la base de donnees
+	 * @param education : l'elmt a ajouter
+	 * @return l'elmt ajoute
+	 */
+	@PostMapping(value ="/AddResearcher")
+	public Researcher createResearcher(@RequestBody Map<String, Object> request) {
+		return rs.saveResearcher(request);
+	}
+	
+	
+	/**
+	 * Update - Update an existing element
+	 * @param id - The id of the element
+	 * @param researcher - The element
+	 * @return
+	 */
+	@PutMapping("/updateResearcher/{id}")
+	public Researcher updateResearcher(@PathVariable("id") final Integer id, @RequestBody Map<String, Object> request) {
+		Optional<Researcher> e = researchRepo.findById(id);
+		if(e.isPresent()) {
+			Researcher currentResearcher = e.get();
+			
+			String researcherSurname = (String)request.get("researcherSurname");
+			if(researcherSurname != null) {
+				currentResearcher.setResearcherSurname(researcherSurname);
+			}
+			
+			String researcherName = (String)request.get("researcherName");
+			if(researcherName != null) {
+				currentResearcher.setResearcherName(researcherName);
+			}
+			
+			String researcherEmail = (String)request.get("researcherEmail");
+			if(researcherEmail != null) {
+				currentResearcher.setResearcherEmail(researcherEmail);
+			}
+			
+			researchRepo.save(currentResearcher);
+			return currentResearcher;
+			
+		} else {
+			return null;
+		}
 	}
 }
